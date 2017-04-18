db09f494f5685cd7fd481149cc727652f9d3a0bd
SuvStreet <suvstreet@gmail.com>
2017-04-18 18:20:34 +0300

    Create Создание окна.py

---
+++ Tkinter_help/Создание окна.py
@@ -0,0 +1,6 @@
+from tkinter import *  # импортирование библиотеки tkinter
+
+root = Tk() # создание окна
+root.title("Моё окно.") # заголовок окна
+
+root.mainloop() # запуск цикл обработки событий
